81cf46d360b8861e6a825ed8ad7ea8a2b273cd1c
Glenn McDonald <2371316+glenn-m@users.noreply.github.com>
2019-03-31 22:34:40 +0100

    Adding in xautolock in i3

--- cfg/desktop-i3.nix
+++ cfg/desktop-i3.nix
@@ -11,8 +11,9 @@
         i3 = {
           enable = true;
           extraPackages = with pkgs; [
-            i3status
             i3lock
+            i3status
+            xautolock
           ];
         };
       };
